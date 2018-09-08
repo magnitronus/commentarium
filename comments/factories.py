a6fcc61d05985cb0df284edaf2ae8ce7a5e0e24a
Anton Lyisenkov <anton@lyisenkov.ru>
2018-09-08 21:51:36 +0400

    Fix factories and test data generator

--- comments/factories.py
+++ comments/factories.py
@@ -1,25 +1,10 @@
-import itertools
-import random
-
-from django.contrib.auth import get_user_model
-from factory import Sequence
 from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyText
 
 from comments.models import Comment
-from entities.factories import UserFactory, PostFactory
 
-User = get_user_model()
-
-users = [UserFactory() for _ in range(10)]
-
-posts = [PostFactory() for _ in range(10)]
-
-entities = posts + users
 
 class CommentFactory(DjangoModelFactory):
-    user = Sequence(lambda _: random.choice(users))
-    entity = Sequence(lambda _: random.choice(entities))
     text = FuzzyText()
 
     class Meta:
--- comments/management/commands/gen_testdata.py
+++ comments/management/commands/gen_testdata.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 from django.db.models.expressions import Random
@@ -5,6 +7,8 @@ from tqdm import tqdm
 
 from comments.factories import CommentFactory
 from comments.models import Comment
+from entities.factories import UserFactory, PostFactory
+from entities.models import Post
 
 User = get_user_model()
 
@@ -27,26 +31,45 @@ class Command(BaseCommand):
             help='Needed nodes count',
         )
 
+        parser.add_argument(
+            '--cleanup',
+            action='store_true',
+            help='Cleanup database before generate data'
+        )
+
     def handle(self, *args, **options):
         count = options.get('count')
         depth = options.get('depth')
         if count / depth < 10:
             raise Exception('The count should be at least 10 times larger than the depth')
         self.stdout.write('Run  "%s"...' % __name__.split('.')[-1])
+        if options.get('cleanup'):
+            self.stdout.write('Cleanup database')
+            User.objects.all().delete()
+            Post.objects.all().delete()
+            Comment.objects.all().delete()
+        users = [UserFactory() for _ in range(10)]
+        entities = users + [PostFactory() for _ in range(10)]
         roots_count = int(count // depth // 2)
         self.stdout.write('Create tree')
         with tqdm(total=roots_count*depth) as pbar:
             for root_idx in range(roots_count):
                 parent = None
                 for depth_idx in range(depth):
-                    parent = CommentFactory.create(parent=parent)
+                    cf_kwargs = dict(parent=parent, user=random.choice(users))
+                    if parent is None:
+                        cf_kwargs['entity'] = random.choice(entities)
+                    parent = CommentFactory.create(**cf_kwargs)
                     pbar.update()
         remain_count = count - roots_count*depth
-        random_comments = Comment.objects.annotate(random=Random()).order_by('random')[:remain_count]
         remain_comments = []
         self.stdout.write('Create remains')
         with tqdm(total=remain_count) as pbar:
-            for random_comment in random_comments:
-                remain_comments.append(CommentFactory.build(parent=random_comment))
+            for random_comment in range(remain_count):
+                remain_comments.append(
+                    CommentFactory.build(
+                        parent=Comment.objects.annotate(
+                            random=Random()).order_by('random').first(),
+                        user=random.choice(users)))
                 pbar.update()
             Comment.objects.bulk_create(remain_comments, batch_size=100)
